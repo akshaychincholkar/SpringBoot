88a897c251d65a6c4706d637eaff0ee5b354532c
bittu <akshaychincholkar@gmail.com>
2021-03-23 09:02:02 +0530

    REST web services

--- src/main/java/com/game/springboot/basics/springbootlearning/SpringbootlearningApplication.java
+++ src/main/java/com/game/springboot/basics/springbootlearning/SpringbootlearningApplication.java
@@ -2,12 +2,16 @@ package com.game.springboot.basics.springbootlearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootlearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootlearningApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootlearningApplication.class, args);
+		for (String beans : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(beans+"\t");
+		}
 	}
 
 }
